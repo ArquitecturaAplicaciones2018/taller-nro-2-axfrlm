6990818f1a3b6616bb97a3930478b51f1c5e4ee1
Alex Fernando León Montaño <axfernandoleon@outlook.com>
2018-10-26 17:29:19 -0500

    pruebas unitarias de los metodos rational

--- main/src/test/java/TestRational.java
+++ main/src/test/java/TestRational.java
@@ -1,2 +1,43 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
 public class TestRational {
+    Rational r = new Rational(1,2);
+    Rational r2 = new Rational(1,4);
+
+    public TestRational() {
+
+    }
+
+    @Test
+    public void isLessThanTest() {
+
+        assertEquals(false, r.isLessThan(r2));
+    }
+
+    @Test
+    public void getGreaterTest() {
+
+        assertEquals("1/2",r.getGreater(r2).toString());
+    }
+    @Test
+    public void plusTest() {
+
+        assertEquals("3/4", r.plus(r2).toString());
+    }
+    @Test
+    public void minusTest() {
+        assertEquals("1/4", r.minus(r2).toString());
+    }
+    @Test
+    public void timesTest() {
+        assertEquals("1/8", r.times(r2).toString());
+    }
+    @Test
+    public void dividesTest() {
+        assertEquals("2", r.divides(r2).toString());
+    }
+
+
 }
